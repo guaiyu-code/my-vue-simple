1cd045defc494197443e26a42fe7ab462a176501
huanxinchun <173315279@qq.com>
2022-04-21 19:44:26 +0800

    push

---
+++ vue-app-test-1/src/components/NewDay0421.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+      <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewDay0421',
+  props: {
+    msg: String
+  }
+}
+</script>
+
