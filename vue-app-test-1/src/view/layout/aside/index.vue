78af7d22adeba7595ae20998b1a235ca47f3cffb
huanxinchun <173315279@qq.com>
2022-04-25 20:15:23 +0800

    push

--- vue-app-test-1/src/view/layout/aside/index.vue
+++ vue-app-test-1/src/view/layout/aside/index.vue
@@ -2,7 +2,7 @@
     <el-scrollbar>
         <el-menu default-active="">
           <el-submenu index="">
-            <el-menu-item index=""> 12121</el-menu-item>
+            <el-menu-item index=""> {{ Test11 }}</el-menu-item>
           </el-submenu>
           <el-menu-item index=""> 312121 </el-menu-item>
         </el-menu>
@@ -13,5 +13,10 @@
 export default {
     name : 'LaoutAside'
 }
+</script>
+
+<script setup>
+    console.log('env', import.meta.env.VITE_CLI_PORT)
+    // const Test11 = import.meta.env.VITE_CLI_PORT
 </script>
 
